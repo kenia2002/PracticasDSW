aa79d163dab436e02af94cb60d8b0e72c67f7c51
kenia2002 <135262346+Kenia765@users.noreply.github.com>
2023-10-03 18:40:29 -0700

    Iniciamos Practica 22 Y 23, UTILIZANDO JAVASCRIPT

---
+++ PRACTICA22/Ejercicio22.js
@@ -0,0 +1,30 @@
+
+var num1= prompt("Ingrese el primer numero"); //Para que el usuario ingrese cosas
+var num2 =prompt("Ingrese el segundo numero"); //Para que el usuario ingrese cosas 
+
+var numero1 = parseFloat (num1);
+var numero2 = parseFloat (num2);
+
+var resultadoSuma= (numero1 + numero2); 
+var resultadoResta= (numero1 - numero2); 
+var resultadoMultiplicacion=(numero1 * numero2); 
+var resultadoDivision= (numero1 / numero2); 
+
+alert ("El resultado de la suma es: "+resultadoSuma); 
+alert ("El resultado de la resta es: "+resultadoResta);  
+alert ("El resultado de la multiplicacion es: "+resultadoMultiplicacion);  
+alert ("El resultado de la division es: "+resultadoDivision);  
+
+/*var mensaje= "Bienvenido a nuestro Sitio Web";
+var texto_1= "texto comillas dobles,";
+var texto_2= 'texto comidas simples ....';
+
+alert (mensaje + texto_1 + texto_2);
+
+var clienteRegistrado = false; 
+var ivaIncluido = true; 
+
+alert (clienteRegistrado);*/
+
+/*var array= ['Tijuana', 'Rosarito', 'Ensenada', 'Mexicalia']; 
+alert (array); */
---
+++ PRACTICA23/Ejercicio23.js
@@ -0,0 +1,29 @@
+var valores = [true, 10, false, "codigo", "herramientas",7];
+
+//Determinar cual de los dos elementos de texto es mayor
+var resultado = valores [3] > valores [4]; 
+alert ("El texto mayor es: " + resultado); 
+
+
+//Utilizando exclusivamente los dos valores booleanos del array, determinar los operadores
+ //necesarios para obtener un resultado true y otro false
+var valor1 = valores[3];
+var valor2 = valores[4];
+
+//Determinar el resultado de las cinco operaciones matematicas realizadas
+//con los dos elemenos numericos 
+var num1 = valores[1];
+var num2 = valores[5];
+
+var suma = num1 + num2;
+alert("La suma de la operacion es: " + suma);
+
+var resta = num1 - num2;
+alert("La resta de la operacion es: " + resta);
+
+var multiplicacion = num1 * num2;
+alert("La multiplicacion de la operacion es: " + multiplicacion);
+
+var division = num1 / num2;
+alert("La division de la operacion es: " + division);
+
