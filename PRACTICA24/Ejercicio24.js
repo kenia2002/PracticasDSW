b79829d146d6641d55e7b37825e93f67b268f110
kenia2002 <135262346+Kenia765@users.noreply.github.com>
2023-10-09 19:37:27 -0700

    Iniciamos Practica 24

---
+++ PRACTICA24/Ejercicio24.js
@@ -0,0 +1,11 @@
+
+var num1 = 5; 
+var num2 =8;
+
+if (num1<=num2){
+alert ("Num1 no es mayor que Num2");
+} else if (num2<=0) {
+    alert ("Num2 es positivo"); 
+} else {
+    ("Incrementar uno en num1, no lo hace mayor que num2");
+}
